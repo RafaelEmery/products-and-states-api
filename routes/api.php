1a5d5f44499fac0c23c99c75e2261fc8afdc88b8
Rafael Emery <rafael.emerycade@gmail.com>
2021-03-22 18:34:53 -0300

    feat(states): basic structure created

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\StateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,9 @@ use App\Http\Controllers\Api\ProductController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::resource('/products', ProductController::class);
+Route::prefix('v1')->group(function () {
+    Route::resource('products', ProductController::class);
+    
+    Route::get('states', [ StateController::class, 'index']);
+});
 
